cacfeea15279648ee941e1ff5edb12f3d02fd5ec
Jason Crawford <daxapps777@gmail.com>
2017-08-06 06:05:25 -0500

    Add google-map-react

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './signup-form.css';
+import "./signup-form.css";
 
 export function SignupForm() {
-
-    return (
-        <form method="POST" className="forms">
-        	<div className="container">
-			    <h5>Find Your Pet Doc Today</h5>
-			    <input type="text" placeholder="Username" name="username" required></input>
-			    <input type="text" placeholder="Enter Email" name="email" required></input>
-			    <input type="password" placeholder="Enter Password" name="psw" required></input>
-			    <div>
-			      <button type="button"  className="cancelbtn">Cancel</button>
-			      <button type="submit" className="signupbtn">Sign Up</button>
-			    </div>
+	return (
+		<form method="POST" className="forms">
+			<div className="container">
+				<h5>Find Your Pet Doc Today</h5>
+				<input type="text" placeholder="Enter Username" name="username" required />
+				<input type="text" placeholder="Enter Email" name="email" required />
+				<input
+					type="password"
+					placeholder="Enter Password"
+					name="psw"
+					required
+				/>
+				<div>
+					<button type="button" className="cancelbtn">
+						Cancel
+					</button>
+					<button type="submit" className="signupbtn">
+						Sign Up
+					</button>
+				</div>
 			</div>
-        </form>
-    );
-};
-
+		</form>
+	);
+}
 
 export default SignupForm;
