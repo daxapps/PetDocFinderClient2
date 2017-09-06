6cf99d1cebf3f343a59e0ba4deb1270795297460
Jason Crawford <daxapps777@gmail.com>
2017-09-06 12:02:49 -0500

    Change API BASE URL

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8080/api"
+    "https://pet-doc-finder-api.herokuapp.com/api"
 };
