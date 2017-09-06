1fe20b2c9d0ad016b7ef487591d9c8dcaf905099
Jason Crawford <daxapps777@gmail.com>
2017-09-06 11:34:59 -0500

    wip

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8080/api"
+    "https://dashboard.heroku.com/apps/pet-doc-finder-api/api"
 };
