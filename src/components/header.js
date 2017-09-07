e30d86165f0d347c3e957d6da2305743713de63e
Jason Crawford <daxapps777@gmail.com>
2017-09-07 17:01:53 -0500

    Change header image

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,6 @@ import "./header.css";
 export function Header() {
 	return (
 		<header>
-			<h1>Pet Doc Finder</h1>
-			<h2>Find the best veterinarian for your loved one</h2>
 			<img src={Banner} alt="cute pupies and kittens" />
 		</header>
 	);
