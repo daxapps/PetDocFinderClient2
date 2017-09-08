02514c818c0c7d61583cdf6b809c6ab6ebc93842
Jason Crawford <daxapps777@gmail.com>
2017-09-08 12:39:19 -0500

    Add screenshot to landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -4,6 +4,8 @@ import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
 import Header from './header';
+import ScreenShot from "./screen-shot.jpg";
+
 
 export function LandingPage(props) {
   // If we are logged in redirect straight to the user's dashboard
@@ -22,6 +24,9 @@ export function LandingPage(props) {
           location of your choice. Compare reviews and prices, so that you can
           choose which one will be the best to take care of your loved one.
         </p>
+        <div className="screen-shot">
+          <img src={ScreenShot} alt="screen shot of map and vet list" />
+        </div>
       </section>
       <section>
         <h3>Help Others Make an Informed Decision</h3>
