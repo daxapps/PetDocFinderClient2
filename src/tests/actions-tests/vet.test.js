c641653644f4fcac8f7ecc8b2da471df92b8235a
Jason Crawford <daxapps777@gmail.com>
2017-09-12 19:49:45 -0500

    WIP actions tests

--- src/tests/actions-tests/vet.test.js
+++ src/tests/actions-tests/vet.test.js
@@ -1,8 +1,12 @@
+import mongoose from 'mongoose';
 import {
 	SHOW_ADD_SERVICE, showAddServiceAction, 
 	FETCH_SERVICE_SUCCESS, fetchServiceSuccess, 
 	GOOGLE_MAPS, googleMaps
 } from '../../actions/vet';
+import Utilities from '../../utilities/utilities';
+
+mongoose.connect('mongodb://localhost/test');
 
 describe('fetchServiceSuccess', () => {
 	it('Should return the action', () => {
@@ -24,16 +28,36 @@ describe('googleMaps', () => {
 });
 
 
-// describe('showAddServiceAction', () => {
-// 	it('Should return the action', () => {
-// 		const obj = {
-// 			googleId: '145145',
-// 			vetname: 'Petsmart'
-// 		};
-// 		const action = showAddServiceAction(obj);
-// 		expect(action.type).toEqual(SHOW_ADD_SERVICE);
-// 		expect(action.payload).toEqual(obj);
-// 	});
-// });
+describe('Vets', () => {
+	var currentVet = null;
+
+	beforeEach(function(done){    
+    // register?????    
+    vet.register("12356432643", "Petsmart", function(doc){      
+      currentVet = doc;      
+      done();    
+    });  
+  });  
+
+  afterEach(function(done){    
+    vet.model.remove({}, function() {      
+      done();    
+    });  
+  });
+
+	it('Should return the vet', (done) => {
+		// const obj = {
+		// 	googleId: '12356432643',
+		// 	vetname: 'Petsmart'
+		// };
+		// const action = showAddServiceAction(obj);
+		// expect(action.type).toEqual(SHOW_ADD_SERVICE);
+		// expect(action.payload).toEqual(obj);
+		vet.findById(currentVet.vetId, function(doc) {
+			doc.vetId.should.equal("12356432643");
+			done();
+		});
+	});
+});
 
 
