61b92a228779d57dd5fd335861a93a293e5e3952
Jason Crawford <daxapps777@gmail.com>
2017-09-26 06:57:02 -0500

    Remove demo login info

--- src/components/login-page.js
+++ src/components/login-page.js
@@ -1,31 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
 
-import LoginForm from './login-form';
+import LoginForm from "./login-form";
 
 export function LoginPage(props) {
-	// If we are logged in redirect straight to the user's dashboard
+  // If we are logged in redirect straight to the user's dashboard
   if (props.loggedIn) {
-    window.scrollTo( 0, 0 );
+    window.scrollTo(0, 0);
     return <Redirect to="/dashboard" />;
   }
 
-	return (
-		<div className="login-page">
-			<section>
+  return (
+    <div className="login-page">
+      <section>
         <h3>Welcome to Pet Doc Finder</h3>
-        <div className="demo-info">
-          <p>To access the demo account, please enter the following credentials.</p>
-          <p>Username: Demo</p>
-          <p>Password: password</p>
-        </div>
         <LoginForm />
         <Link to="/register">Register</Link>
       </section>
-		</div>
-
-		)
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./top-nav.css";
 
 export default function TopNav() {
 	return (
 		<Navbar inverse collapseOnSelect>
-	    <Navbar.Header>
-	      <Navbar.Brand>
-	        <a href="/">Pet Doc Finder</a>
-	      </Navbar.Brand>
-	      <Navbar.Toggle />
-	    </Navbar.Header>
-	    <Navbar.Collapse>
-	      <Nav pullRight>
-	      	<LinkContainer to="/register">
-	        <NavItem eventKey={1}>SignUp</NavItem>
-	        </LinkContainer>
-	        <LinkContainer to="/login">
-	        <NavItem eventKey={2}>Login / Demo</NavItem>
-	        </LinkContainer>
-	      </Nav>
-	    </Navbar.Collapse>
-	  </Navbar>
+			<Navbar.Header>
+				<Navbar.Brand>
+					<a href="/">Pet Doc Finder</a>
+				</Navbar.Brand>
+				<Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+				<Nav pullRight>
+					<LinkContainer to="/register">
+						<NavItem eventKey={1}>SignUp</NavItem>
+					</LinkContainer>
+					<LinkContainer to="/login">
+						<NavItem eventKey={2}>Login</NavItem>
+					</LinkContainer>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 }
